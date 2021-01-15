e024e45cc6cd0b74de7d609e5663c1ef914c7fae
Vincent Hullaert <vincent.hullaert@prestataires.boulanger.com>
2021-01-15 12:00:09 +0100

    feat(test): add test containers instead of connect to local mongodb

--- src/main/java/com/boulanger/mongotest/model/Customer.java
+++ src/main/java/com/boulanger/mongotest/model/Customer.java
@@ -2,25 +2,24 @@ package com.boulanger.mongotest.model;
 
 import lombok.Data;
 import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document
 @Data
+@ToString
+@RequiredArgsConstructor
 public class Customer {
 
     @Id
-    public @NonNull String id;
+    public String id;
     public @NonNull String firstName;
     public @NonNull String lastName;
 
     @Indexed(name = "idx_email", unique = true)
     public @NonNull String eMail;
 
-    public Customer(@NonNull String firstName, @NonNull String lastName, @NonNull String eMail) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.eMail = eMail;
-    }
 }
--- src/test/java/com/boulanger/mongotest/repository/CustomerRepositoryTests.java
+++ src/test/java/com/boulanger/mongotest/repository/CustomerRepositoryTests.java
@@ -3,16 +3,47 @@ package com.boulanger.mongotest.repository;
 import com.boulanger.mongotest.model.Customer;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.support.TestPropertySourceUtils;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.util.List;
 
-@SpringBootTest
+import static java.lang.String.format;
+
+@ExtendWith(SpringExtension.class)
+@DataMongoTest
+@ContextConfiguration(initializers = CustomerRepositoryTests.Initializer.class)
+@Testcontainers
 public class CustomerRepositoryTests {
 
     @Autowired
     public CustomerRepository customerRepository;
+
+    //anotation container that are shared between all methods of a test class
+    @Container
+    public static GenericContainer mongoContainer = new GenericContainer("mongo:4.4.2");
+
+    //properties for connecting to docker database
+    static class Initializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+
+        @Override
+        public void initialize(ConfigurableApplicationContext configurableApplicationContext) {
+
+            TestPropertySourceUtils.addInlinedPropertiesToEnvironment(configurableApplicationContext,
+                    format("spring.data.mongodb.uri=mongodb://%s:%s/test", mongoContainer.getContainerIpAddress(), mongoContainer.getMappedPort(27017)));
+            TestPropertySourceUtils.addInlinedPropertiesToEnvironment(configurableApplicationContext,
+                    "spring.data.mongodb.auto-index-creation=true");
+        }
+    }
 
     private Customer joExotic = new Customer("Jo", "Exotic", "jo.exotic@gmail.com");
     private Customer caroleBaskin = new Customer("Carole", "Baskin", "Carole.Baskin@gmail.com");
